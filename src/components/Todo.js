dd7daa779e567a0aa5f85239823886c8ff187379
kurimaron <kurisyun4321@gmail.com>
2020-09-05 07:28:38 +0900

    発表直前のコード

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,12 +3,33 @@ import { useDispatch } from "react-redux";
 // import cx from "classnames";
 import { toggleTodo } from "../redux/actions";
 
+import { FormControlLabel, Checkbox } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  completed_todo: {
+    color: "gray",
+    textDecoration: "line-through",
+  },
+  none: {},
+});
+
 const Todo = ({ todo }) => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   return (
     <li>
-      <input type="checkbox" onClick={() => dispatch(toggleTodo(todo.id))} />
-      {todo && todo.completed ? "オワオワリ" : "未完"} {todo.content}
+      {/* <input type="checkbox" onClick={() => dispatch(toggleTodo(todo.id))} />
+      {todo && todo.completed ? "オワオワリ" : "未完"} {todo.content} */}
+
+      <FormControlLabel
+        control={<Checkbox />}
+        label={todo.content}
+        onChange={() => dispatch(toggleTodo(todo.id))}
+        className={
+          todo && todo.completed ? classes.completed_todo : classes.none
+        }
+      />
     </li>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-import "./index.css";
+// import "./index.css";
 import TodoApp from "./TodoApp";
 
+// import Container from "@material-ui/core/Container";
+import { Container, Box } from "@material-ui/core";
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <TodoApp />
-    </Provider>
+    <Container maxWidth="sm">
+      <Box bgcolor="#dcedc8">
+        <Provider store={store}>
+          <TodoApp />
+        </Provider>
+      </Box>
+    </Container>
   </React.StrictMode>,
   document.getElementById("root")
 );
